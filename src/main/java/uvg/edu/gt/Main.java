dc113222e1ae4830ed960e6a26192fed38c23acc
Josero31 <san231221@uvg.edu.gt>
2024-04-29 11:09:09 -0600

    Creacion de carpeta

---
+++ src/main/java/uvg/edu/gt/Main.java
@@ -0,0 +1,26 @@
+package uvg.edu.gt;
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        BinaryTree tree = new BinaryTree();
+
+        Scanner diccionario = new Scanner(new File("diccionario.txt"));
+        while (diccionario.hasNext()) {
+            String[] words = diccionario.nextLine().replaceAll("[()]", "").split(", ");
+            tree.insert(words[0], words[1]);
+        }
+        diccionario.close();
+
+        tree.inorder();
+
+        Scanner texto = new Scanner(new File("texto.txt"));
+        while (texto.hasNext()) {
+            String word = texto.next();
+            System.out.print(tree.search(word) + " ");
+        }
+        texto.close();
+    }
+}
