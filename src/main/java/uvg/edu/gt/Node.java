b7ea932a68bbacdb4b2e3986e36859efc233e21e
Josero31 <san231221@uvg.edu.gt>
2024-04-29 11:14:42 -0600

    Creacion de carpeta

---
+++ src/main/java/uvg/edu/gt/Node.java
@@ -0,0 +1,13 @@
+package uvg.edu.gt;
+
+public class Node {
+    String key;
+    String value;
+    Node left, right;
+
+    public Node(String item, String value) {
+        key = item;
+        this.value = value;
+        left = right = null;
+    }
+}
